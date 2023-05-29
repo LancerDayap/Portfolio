adbf019d5f00b6efe3b272c6e4f1ae9d903b9118
Lancer <rhen.rd3@gmail.com>
2023-05-29 20:01:02 +0800

    added responsiveness

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,22 +1,31 @@
+import { Carousel } from "react-bootstrap"
 
 const Project = () => {
-    return (
-      <div className="projects">
-          <h2>Projects</h2>
+  return (
+    <div className="projects">
+      <h2>Projects</h2>
+      <Carousel>
+        <Carousel.Item>
           <div className="project">
             <h3>Shuriken</h3>
             <p>Description of project 1</p>
           </div>
+        </Carousel.Item>
+        <Carousel.Item>
           <div className="project">
             <h3>Movies for Junkies</h3>
             <p>Description of project 2</p>
           </div>
+        </Carousel.Item>
+        <Carousel.Item>
           <div className="project">
             <h3>Elegant Silver</h3>
             <p>Description of project 3</p>
           </div>
-        </div>
-    )
-  }
-  
-  export default Project
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Project;
