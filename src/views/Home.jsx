0bda7e3f2dccafd7305c9ff25e0974a67f437fea
Lancer <rhen.rd3@gmail.com>
2023-05-29 14:35:55 +0800

    build

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import About from '../components/About'
 import Hero from '../components/Hero'
-import Project from '../components/project'
+import Project from "../components/Project";
 import Footer from "../components/Footer"
 
 const Home = () => {
